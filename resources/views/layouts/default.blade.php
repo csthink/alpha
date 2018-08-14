f5fd229cc45f4e449be5a7f8e85bd7ef163a1468
csthink <security.2009@live.cn>
2018-08-14 17:11:34 +0800

    finish signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -13,6 +13,7 @@
 
 <div class="container">
     <div class="col-md-offset-1 col-md-10">
+        @include('common._message')
         @yield('content')
         @include('layouts._footer')
     </div>
